f2769b72faed402cc6f5f1eee67ba128b8310596
Shayan Faghihi <s.faghihi@bettechnology.com>
2024-03-18 10:30:59 +0330

    Edite Apps List for test

--- components/appBuilders/appsList.js
+++ components/appBuilders/appsList.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import AppBox from "./appBox";
 
 import classes from "./appsList.module.css";
-import CompareFooter from "../compare/compareFooter";
+import CompareTitleColumn from "../compare/titleColumn";
 
 export default function AppsList({ appBuilders }) {
   const [compareList, setCompareList] = useState([]);
@@ -50,7 +50,7 @@ export default function AppsList({ appBuilders }) {
           />
         ))}
       </section>
-      <CompareFooter />
+      <CompareTitleColumn />
     </>
   );
 }
